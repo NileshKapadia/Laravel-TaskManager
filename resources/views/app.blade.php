60e079c464c259b10200f57d49bfefe719b61cef
NileshKapadia <nileshkapadiya04@gmail.com>
2025-02-24 14:09:52 -0500

    update

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -12,7 +12,6 @@
         <div class="container mt-5">
             @yield('content')
         </div>
-
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
         <script>
             $(function() {
